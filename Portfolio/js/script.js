8e3d8ee03588d590d33a366e8210e2ceba8b037e
Gerlison <franciscogerlison1@gmail.com>
2018-07-19 18:14:32 -0300

    d

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -10,28 +10,28 @@
         
         
         
-        $('.owl-carousel').owlCarousel({
-            loop:true,
-            margin:10,
-            nav: true,
-            // autoWidth:true,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:true
-                },
-                600:{
-                    items:2,
-                    nav:false
-                },
-                1000:{
-                    items:4,
-                    nav:true,
-                    loop:false
-                }
-            }
-        });
+        // $('.owl-carousel').owlCarousel({
+        //     loop:true,
+        //     margin:10,
+        //     nav: true,
+        //     // autoWidth:true,
+        //     responsiveClass:true,
+        //     responsive:{
+        //         0:{
+        //             items:1,
+        //             nav:true
+        //         },
+        //         600:{
+        //             items:2,
+        //             nav:false
+        //         },
+        //         1000:{
+        //             items:4,
+        //             nav:true,
+        //             loop:false
+        //         }
+        //     }
+        // });
 
 
 
